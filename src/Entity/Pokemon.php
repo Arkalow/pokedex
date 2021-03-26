d9f1844209493510ab556dce95e975f66108f3f4
Omer Lakraa <omer@arkalo.ovh>
2021-03-26 14:41:23 +0100

    Add Pokemon, Generation, Type Entities

---
+++ src/Entity/Pokemon.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PokemonRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PokemonRepository::class)
+ */
+class Pokemon
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $numero;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $vie;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $attaque;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $defense;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $legendaire;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Type::class, inversedBy="pokemon")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $type1;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Type::class, inversedBy="pokemon2")
+     */
+    private $type2;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Generation::class, inversedBy="pokemon")
+     */
+    private $generation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNumero(): ?int
+    {
+        return $this->numero;
+    }
+
+    public function setNumero(int $numero): self
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getVie(): ?int
+    {
+        return $this->vie;
+    }
+
+    public function setVie(int $vie): self
+    {
+        $this->vie = $vie;
+
+        return $this;
+    }
+
+    public function getAttaque(): ?int
+    {
+        return $this->attaque;
+    }
+
+    public function setAttaque(int $attaque): self
+    {
+        $this->attaque = $attaque;
+
+        return $this;
+    }
+
+    public function getDefense(): ?int
+    {
+        return $this->defense;
+    }
+
+    public function setDefense(int $defense): self
+    {
+        $this->defense = $defense;
+
+        return $this;
+    }
+
+    public function getLegendaire(): ?bool
+    {
+        return $this->legendaire;
+    }
+
+    public function setLegendaire(bool $legendaire): self
+    {
+        $this->legendaire = $legendaire;
+
+        return $this;
+    }
+
+    public function getType1(): ?Type
+    {
+        return $this->type1;
+    }
+
+    public function setType1(?Type $type1): self
+    {
+        $this->type1 = $type1;
+
+        return $this;
+    }
+
+    public function getType2(): ?Type
+    {
+        return $this->type2;
+    }
+
+    public function setType2(?Type $type2): self
+    {
+        $this->type2 = $type2;
+
+        return $this;
+    }
+
+    public function getGeneration(): ?Generation
+    {
+        return $this->generation;
+    }
+
+    public function setGeneration(?Generation $generation): self
+    {
+        $this->generation = $generation;
+
+        return $this;
+    }
+}
