bc95fde78e3f781b6ef8d5a02ad71e9aab93bb66
Omer Lakraa <omer@arkalo.ovh>
2021-04-13 18:14:50 +0200

    OUI

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ apt install -y sudo nginx php-fpm sqlite3 php-sqlite3 php-zip unzip php-xml git
 cd /var/www/html
 
 # composer
-php -r copy('https://getcomposer.org/installer', 'composer-setup.php');
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php composer-setup.php
-php -r unlink('composer-setup.php');
+php -r "unlink('composer-setup.php');"
 mv composer.phar /usr/local/bin/composer
 git clone "https://git.arkalo.ovh/Omer/project.git" /var/www/html/webapp
 
